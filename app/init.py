8fd871a2ac589c10fde7f817267c17d59920b75a
Minemaster656 <glebbasmanov123@gmail.com>
2025-10-29 11:43:57 +0400

    my first template

---
+++ app/init.py
@@ -0,0 +1,7 @@
+from flask import Flask, Blueprint, request, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def root():
+    return render_template("index.html")
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from colorama import Fore, Style, Back
 from app.core import db
+# from flask import Flask, Blueprint, render_template
+from app.init import app
+
 
 print(f"{Fore.GREEN}LAUNCHING {Style.BRIGHT}{Fore.MAGENTA}Do{Fore.CYAN}Dep{Fore.LIGHTBLUE_EX} 2{Style.RESET_ALL}{Fore.GREEN}...{Style.RESET_ALL}")
 
+
+
 if __name__ == '__main__':
-    
     try:
         print(f"{Fore.GREEN}Hello, world!{Style.RESET_ALL}")
+        app.run(host="0.0.0.0", port=34778, debug=True)
         
     finally:
         print(f"{Fore.RED}Exiting...{Style.RESET_ALL}")
