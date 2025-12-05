fe39e399e5a50f3793456d48e2099a3e5734580f
Minemaster656 <glebbasmanov123@gmail.com>
2025-12-05 15:37:07 +0400

    пофиксил случайное выжигание глаз светлой темой в профилях

--- app/src/other/other.py
+++ app/src/other/other.py
@@ -8,11 +8,20 @@ from app.core.jwt_service import create_access_token, decode_access_token, requi
 bp = Blueprint('other', __name__, url_prefix="/api/v1/other")
 bp_nopref = Blueprint('other_nopref', __name__, url_prefix="/")
 
-@bp_nopref.get("/profile/<uid>")
-def profile(uid):
+@bp_nopref.get("/profile/id/<uid>")
+def profile_uid(uid):
     conn, cur = db.get_cursor()
     uname = cur.execute("SELECT name FROM users WHERE id = ?", (uid, )).fetchone()
     if uname:
-        return render_template("pages/profile.html", name=uname)
+        return render_template("pages/profile.html", name=uname[0], uid=uid)
     else:
-        return render_template("pages/404.html")
+        return render_template("pages/404.html", message=f"Пользователь #{uid} не найден :(")
+
+@bp_nopref.get("/profile/<tag>")
+def profile(tag):
+    conn, cur = db.get_cursor()
+    uname = cur.execute("SELECT name, id FROM users WHERE login = ?", (tag, )).fetchone()
+    if uname:
+        return render_template("pages/profile.html", name=uname[0], uid=uname[1])
+    else:
+        return render_template("pages/404.html", message=f"Пользователь @{tag} не найден :(")
